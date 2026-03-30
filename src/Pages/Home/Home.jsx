107ecc8d494a23e0908bf73421648424c2a9bd61
kamruzzaman1234 <dmkkaarr.labib980@gmail.com>
2026-03-31 00:28:32 +0600

    added feature code

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Banner from "../../components/Banner/Banner";
+import Feature from "../../components/Feature/Feature";
 
 const Home = ()=>{
     return(
         <div className="">
                 <Banner></Banner>
+                <Feature></Feature>
         </div>  
     )
 }
---
+++ src/components/Feature/Feature.jsx
@@ -0,0 +1,26 @@
+const Feature = ()=>{
+    return(
+        <div className="py-[80px]">
+            <div className="max-w-screen-xl mx-auto">
+                <div className="flex flex-col md:flex-col gap-8">
+                    <div>
+                        <h2 className="text-[18px] md:text-2xl font-bold text-black uppercase">Feature Work</h2>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-6">
+                        <div className="h-64">
+                            <img src="https://i.ibb.co.com/pvP2ng1f/h5.jpg" alt="" className="w-full h-full object-cover"/>
+                        </div>
+                        <div className="h-64"> F
+                            <img src="https://i.ibb.co.com/YBHjfbtt/pexels-pixabay-262048.jpg" alt="" className="w-full h-full " />
+                        </div>
+                        <div className="h-64">
+                            <img src="https://i.ibb.co.com/pvP2ng1f/h5.jpg" alt="" className="w-full h-full object-cover" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Feature;
